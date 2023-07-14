a4ea497c0a3918d87597feac78e618cd69c5aeb1
Firzaazz <113733978+Firzaazz@users.noreply.github.com>
2023-07-15 02:10:31 +0700

    Add files via upload

--- Mobile/PH1/Scrn/Profile.js
+++ Mobile/PH1/Scrn/Profile.js
@@ -1,60 +1,106 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet, TouchableOpacity, Text, Dimensions } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as ImagePicker from 'expo-image-picker';
+import HeadProf from '../Component/HeadProf';
 
-const Profile = () => {
-  return (
-    <View>
-      <Text style={styles.heading}>Profil Saya</Text>
-      <Image
-        source={require('../assets/Logokosongan.png')} // Ganti dengan sumber gambar yang ingin ditambahkan di atas gambar latar belakang
-        style={styles.overlayImage}
-      />
-      <Text style={styles.label}>Identitas</Text>
-      <Text style={styles.info}>Nama: User</Text>
-      <Text style={styles.info}>Umur: 25 tahun</Text>
-      <Text style={styles.info}>Pekerjaan: Web Developer</Text>
-      {/* tambahkan informasi profil lainnya sesuai kebutuhan */}
-    </View>
-  );
-};
+class Profile extends Component {
+  openCamera = async () => {
+    const { status } = await Camera.requestCameraPermissionsAsync();
+    if (status === 'granted') {
+      const image = await ImagePicker.launchCameraAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        allowsEditing: false,
+        aspect: [4, 3],
+      });
+
+      if (!image.cancelled) {
+        console.log('Image URI:', image.uri);
+        // Lakukan operasi dengan gambar sesuai kebutuhan Anda
+      }
+    } else {
+      console.log('Camera permission denied');
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.flex1}>
+          <HeadProf />
+        </View>
+        <View style={styles.flex2}>
+          <View style={styles.buttonContainer}>
+            <Text style={styles.identityText}>Identity</Text>
+            <TouchableOpacity style={styles.button} onPress={this.openCamera}>
+              <Text style={styles.buttonText}>ID Verification</Text>
+            </TouchableOpacity>
+            <Text style={styles.settingText}>Setting</Text>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Tombol 2</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Tombol 3</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Tombol 4</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Tombol 5</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Tombol 6</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const { width } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-  heading: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginTop: 20,
-    marginBottom: 10,
-    textAlign: 'center',
+  container: {
+    flex: 1,
   },
-  profileImage: {
-    width: 200,
-    height: 200,
-    alignSelf: 'center',
-    borderRadius: 100,
+  flex1: {
+    flex: 1,
+  },
+  flex2: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: -200,
+  },
+  buttonContainer: {
+    width: '100%',
     marginBottom: 20,
   },
-  label: {
-    fontSize: 18,
+  identityText: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    marginLeft: 10,
+  },
+  button: {
+    width: '100%',
+    height: 50,
+    backgroundColor: '#ffff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 2,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: 'black',
+  },
+  settingText: {
+    fontSize: 15,
     fontWeight: 'bold',
     marginTop: 10,
-    marginLeft: 20,
-  },
-  info: {
-    fontSize: 16,
-    marginLeft: 20,
-    marginBottom: 10,
-  },
-  overlayImage: {
-    width: 560,
-    height: 300,
-    alignSelf: 'center',
-    borderRadius: 50,
-    marginBottom: 20,
-    position: 'absolute',
-    top: '50%',
-    
-    
-    transform: [{ translateX: -100 }, { translateY: -100 }],
+    marginLeft: 10,
   },
 });
 
